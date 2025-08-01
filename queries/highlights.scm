6f679f587eb5ea42f0f9f76395100af0964a78c1
oscar-benderstone <oscarbenderstone@gmail.com>
2025-07-31 18:42:05 -0600

    Added queries

---
+++ queries/highlights.scm
@@ -0,0 +1,18 @@
+(comment) @comment
+
+(heading) @text.title
+(wikilink) @link_uri
+(external_link) @link_uri
+(wikilabel) @link_text
+(category) @label
+(file) @label
+(image) @label
+
+[
+    (bl1)
+    (bl2)
+    (bl3)
+    (nl1)
+    (nl2)
+    (nl3)
+] @punctuation.list_marker
